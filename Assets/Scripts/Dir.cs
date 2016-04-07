cc0383325abd12507094b45055de12b5e2307264
Norio Kobayashi <noriokob@gmail.com>
2016-04-07 22:45:52 +0900

    Add comment

--- Assets/Scripts/Dir.cs
+++ Assets/Scripts/Dir.cs
@@ -1,10 +1,11 @@
-﻿public enum Dir {
-    N,
-    NE,
-    E,
-    SE,
-    S,
-    SW,
-    W,
-    NW,
+﻿// 方向
+public enum Dir {
+    N,  // 北
+    NE, // 北東
+    E,  // 東
+    SE, // 南東
+    S,  // 南
+    SW, // 南西
+    W,  // 西
+    NW, // 北西
 }
