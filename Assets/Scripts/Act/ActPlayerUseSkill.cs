460da6e675e9b0e4fc6ed59061673aff22723023
Norio Kobayashi <noriokob@gmail.com>
2016-04-17 19:03:08 +0900

    全体ダメージでダメージアニメを並列に実行

--- Assets/Scripts/Act/ActPlayerUseSkill.cs
+++ Assets/Scripts/Act/ActPlayerUseSkill.cs
@@ -1,35 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class DamageWait {
+	public bool AnimationFinished { get; private set; }
+
+	private CharacterBase _target;
+
+	private IEnumerator Anim() {
+		var dmg = 9999;
+		_target.DamageHp(dmg);
+
+		var pos = _target.Position;
+		pos.y -= 0.09f;
+		yield return EffectAnim.PopupWhiteDigits(dmg, pos, () => {});
+		if (_target.Hp <= 0) {
+			var obj = Resources.Load("Prefabs/Animations/dead");
+			var gobj = (GameObject)GameObject.Instantiate(obj, _target.Position, Quaternion.identity);
+			_target.Destroy();
+			while (gobj != null) {
+				yield return null;
+			}
+		}
+		AnimationFinished = true;
+	}
+
+	public DamageWait(CharacterBase target, MainSystem sys) {
+		_target = target;
+		sys.StartCoroutine(Anim());
+	}
+}
 
 public class ActPlayerUseSkill : Act {
 	private CharacterBase[] _targets;
 
 	public ActPlayerUseSkill(Player player, CharacterBase[] targets) : base(player) {
 		_targets = targets;
-
 	}
 
-	// TODO:全てのアニメを同時に実行する
-	private IEnumerator DamageAnim() {
-		for (int i = 0; i < _targets.Length; i++) {
-			var dmg = 9999;
-			_targets[i].DamageHp(dmg);
-
-			var pos = _targets[i].Position;
-			pos.y -= 0.09f;
-			yield return EffectAnim.PopupWhiteDigits(dmg, pos, () => {});
-			if (_targets[i].Hp <= 0) {
-				var obj = Resources.Load("Prefabs/Animations/dead");
-				var gobj = (GameObject)GameObject.Instantiate(obj, _targets[i].Position, Quaternion.identity);
-				_targets[i].Destroy();
-				while (gobj != null) {
-					yield return null;
-				}
-			}
-		}
-	}
-
-	private IEnumerator Anim() {
+	private IEnumerator Anim(MainSystem sys) {
 		var obj = Resources.Load("Prefabs/Animations/aura");
 		var gobj = (GameObject)GameObject.Instantiate(obj, Actor.Position, Quaternion.identity);
 		while (gobj != null) {
@@ -42,13 +51,26 @@ public class ActPlayerUseSkill : Act {
 			yield return null;
 		}
 
-		yield return DamageAnim();
+		var damageAnims = new List<DamageWait>();
+		for (int i = 0; i < _targets.Length; i++) {
+			damageAnims.Add(new DamageWait(_targets[i], sys));
+		}
+
+		while (true) {
+			bool finished = true;
+			foreach (var d in damageAnims) {
+				finished = finished && d.AnimationFinished;
+			}
+
+			if (finished) break;
+			yield return null;
+		}
 
 		AnimationFinished = true;
 	}
 
 	public override void RunAnimation(MainSystem sys) {
-		sys.StartCoroutine(Anim());
+		sys.StartCoroutine(Anim(sys));
 	}
 
 	public override void RunEffect(MainSystem sys) {
