bc65c293c6315d41b4f01d3a7138275279910be8
Norio Kobayashi <noriokob@gmail.com>
2016-04-17 22:22:15 +0900

    攻撃を受けたときに攻撃者の方を向く

--- Assets/Scripts/Act/ActEnemyAttack.cs
+++ Assets/Scripts/Act/ActEnemyAttack.cs
@@ -21,6 +21,9 @@ public class ActEnemyAttack : Act {
         // ターゲットの方を向く
         Actor.ChangeDir(Actor.Loc.Toward(_target.Loc));
 
+        // ターゲットは攻撃者の方を向く
+        _target.ChangeDir(_target.Loc.Toward(Actor.Loc));
+
         var dmg = new System.Random().Next(30);
         var pos = _target.Position;
         pos.y -= 0.09f;
