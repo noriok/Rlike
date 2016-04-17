3e92c94799162c2f2d5016b9cf5221b12d5cdbc2
Norio Kobayashi <noriokob@gmail.com>
2016-04-17 16:01:21 +0900

    攻撃時にキャラを移動させる(攻撃アニメーションの代替)

--- Assets/Scripts/Act/ActEnemyAttack.cs
+++ Assets/Scripts/Act/ActEnemyAttack.cs
@@ -9,10 +9,20 @@ public class ActEnemyAttack : Act {
     }
 
     public override void RunAnimation(MainSystem sys) {
+        // 攻撃キャラを、ターゲットとの中間に移動させる(攻撃アニメーションの代替)
+        var src = Actor.Position;
+        var dst = src;
+        dst.x = (src.x + _target.Position.x) / 2;
+        dst.y = (src.y + _target.Position.y) / 2;
+        Actor.Position = dst;
+
         var dmg = new System.Random().Next(30);
         var pos = _target.Position;
         pos.y -= 0.09f;
-        sys.StartCoroutine(EffectAnim.PopupWhiteDigits(dmg, pos, () => AnimationFinished = true));
+        sys.StartCoroutine(EffectAnim.PopupWhiteDigits(dmg, pos, () => {
+            Actor.Position = src;
+            AnimationFinished = true;
+        }));
     }
 
     public override void RunEffect(MainSystem sys) {
--- Assets/Scripts/Act/ActPlayerAttack.cs
+++ Assets/Scripts/Act/ActPlayerAttack.cs
@@ -13,10 +13,20 @@ public class ActPlayerAttack : Act {
             AnimationFinished = true;
         }
         else {
+            // 攻撃キャラを、ターゲットの中間に移動させる(攻撃アニメーションの代替)
+            var src = Actor.Position;
+            var dst = src;
+            dst.x = (src.x + _target.Position.x) / 2;
+            dst.y = (src.y + _target.Position.y) / 2;
+            Actor.Position = dst;
+
             var dmg = new System.Random().Next(30);
             var pos = _target.Position;
             pos.y -= 0.09f;
-            sys.StartCoroutine(EffectAnim.PopupWhiteDigits(dmg, pos, () => AnimationFinished = true));
+            sys.StartCoroutine(EffectAnim.PopupWhiteDigits(dmg, pos, () => {
+                Actor.Position = src;
+                AnimationFinished = true;
+            }));
         }
     }
 
