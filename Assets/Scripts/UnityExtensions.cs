f5af788d11835c08b26dff9378edb12f593e4c35
Norio Kobayashi <noriokob@gmail.com>
2016-04-25 22:06:58 +0900

    add UnityExtensions

---
+++ Assets/Scripts/UnityExtensions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public static class UnityExtensions {
+
+	public static IEnumerator Fade(this Image image, float fm, float to, float duration) {
+		var color = image.color;
+		float elapsed = 0;
+		while (elapsed <= duration) {
+			elapsed += Time.deltaTime;
+			float alpha = Mathf.Lerp(fm, to, elapsed / duration);
+			color.a = alpha;
+			image.color = color;
+			yield return null;
+		}
+		color.a = to;
+		image.color = color;
+	}
+
+	public static IEnumerator Fade(this Text text, float fm, float to, float duration) {
+		var color = text.color;
+		float elapsed = 0;
+		while (elapsed <= duration) {
+			elapsed += Time.deltaTime;
+			float alpha = Mathf.Lerp(fm, to, elapsed / duration);
+			color.a = alpha;
+			text.color = color;
+			yield return null;
+		}
+		color.a = to;
+		text.color = color;
+	}
+}
