c10febc39c1a0619a0e22c270760761ab49d0a48
Norio Kobayashi <noriokob@gmail.com>
2016-04-27 08:39:22 +0900

    trap

--- Assets/Scripts/Map/TrapSummon.cs
+++ Assets/Scripts/Map/TrapSummon.cs
@@ -8,22 +8,26 @@ public class TrapSummon : Trap {
 
 	}
 
+	// TODO:
+	// 周囲八マスからランダムに選ぶ
+	// その位置に敵がいないことを確認
+	private Func<IEnumerator>[] Foo(Loc src, MainSystem sys) {
+		var locs = new List<Loc>();
+		locs.Add(new Loc(src.Row - 1, src.Col));
+		locs.Add(new Loc(src.Row, src.Col + 1));
+		locs.Add(new Loc(src.Row + 1, src.Col + 1));
+
+		List<Func<IEnumerator>> fns = new List<Func<IEnumerator>>();
+		for (int i = 0; i < locs.Count; i++) {
+			int p = i;
+			fns.Add(() => sys.Summon(locs[p]));
+		}
+		return fns.ToArray();
+	}
+
 	// TODO:Run rename
 	public override IEnumerator RunAnimation(CharacterBase sender, MainSystem sys) {
 		var src = sender.Loc;
-
-		// TODO:クロージャが一時変数を共有してしまう問題
-		List<Func<IEnumerator>> fns = new List<Func<IEnumerator>>();
-		fns.Add(() => sys.Summon(new Loc(src.Row - 1, src.Col)));
-		fns.Add(() => sys.Summon(new Loc(src.Row, src.Col + 1)));
-		fns.Add(() => sys.Summon(new Loc(src.Row + 1, src.Col + 1)));
-
-		// foreach (var loc in locs) {
-		// 	int r = loc.Row;
-		// 	int c = loc.Col;
-		// 	var l = new Loc(r, c);
-		// 	fns.Add(() => sys.Summon(l));
-		// }
-		yield return Anim.Par(sys, fns.ToArray());
+		yield return Anim.Par(sys, Foo(src, sys));
 	}
 }
