82b749379254a62a87f0e87a1ba97aa1866a8ed0
Norio Kobayashi <noriokob@gmail.com>
2016-05-16 22:42:03 +0900

    SkillWarp

---
+++ Assets/Scripts/Skill/SkillWarp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillWarp : Skill {
+
+    public override IEnumerator Use(CharacterBase sender, MainSystem sys) {
+        var src = sender.Position;
+        float duration = 0.5f;
+        float elapsed = 0;
+        while (elapsed <= duration) {
+            float y = UTween.Ease(EaseType.InCubic, src.y, src.y + 6, elapsed / duration);
+            sender.Position = new Vector3(src.x, y, 0);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.36f);
+
+        var loc = sys.RandomRoomLoc(sender.Loc);
+        sender.Position = loc.ToPosition();
+        sender.ChangeDir(Dir.S);
+        sender.UpdateLoc(loc);
+        if (sender is Player) { // TODO: player のメソッド内で処理する
+            ((Player)sender).SyncCameraPosition();
+        }
+        yield return new WaitForSeconds(0.4f);
+    }
+}
