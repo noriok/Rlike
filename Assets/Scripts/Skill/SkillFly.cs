a3b1e4581bd50c1b11495ad2315ed19376031b36
Norio Kobayashi <noriokob@gmail.com>
2016-05-22 16:16:10 +0900

    clean

--- Assets/Scripts/Skill/SkillFly.cs
+++ Assets/Scripts/Skill/SkillFly.cs
@@ -2,32 +2,29 @@
 using UnityEngine.Assertions;
 using System.Collections;
 
+// とびつき TODO:rename
 public class SkillFly : Skill {
+
+    private IEnumerator Run(Player player, Loc to, MainSystem sys) {
+        yield return CAction.MovePlayer(player, to);
+
+        // とびついた先が水ならワープ
+        if (sys.IsWater(player.Loc)) {
+            yield return new SkillWarp().Use(player, sys);
+        }
+    }
 
     public override IEnumerator Hit(CharacterBase sender, CharacterBase target, MainSystem sys) {
         Assert.IsTrue(sender is Player);
-
         // ターゲットの 1 歩前まで移動する
         Loc to = target.Loc.Backward(sender.Dir);
-        yield return CAction.MovePlayer((Player)sender, to);
-
-        // とびついた先が水ならワープ
-        if (sys.IsWater(sender.Loc)) {
-            yield return new SkillWarp().Use(sender, sys);
-        }
+        yield return Run((Player)sender, to, sys);
     }
 
     public override IEnumerator Hit(CharacterBase sender, Loc target, MainSystem sys) {
         Assert.IsTrue(sender is Player);
-
         // ターゲットの 1 歩前まで移動する
         Loc to = target.Backward(sender.Dir);
-        yield return CAction.MovePlayer((Player)sender, to);
-
-        // とびついた先が水ならワープ
-        if (sys.IsWater(sender.Loc)) {
-            yield return new SkillWarp().Use(sender, sys);
-        }
+        yield return Run((Player)sender, to, sys);
     }
-
 }
