3f858c65e287653d0fc5d35f6abc9790e17410e1
Norio Kobayashi <noriokob@gmail.com>
2016-05-28 13:16:54 +0900

    indent

--- Assets/Scripts/YesNoDialog.cs
+++ Assets/Scripts/YesNoDialog.cs
@@ -5,23 +5,23 @@ using UnityEngine.UI;
 
 public class YesNoDialog {
 
-	public YesNoDialog(GameObject dialog, string message, Action yesCallback, Action noCallback) {
+    public YesNoDialog(GameObject dialog, string message, Action yesCallback, Action noCallback) {
         dialog.SetActive(true);
         var text = dialog.transform.Find("Text").GetComponent<Text>();
         text.text = message;
 
-		var btnYes = GameObject.Find("Canvas/YesNoDialog/Button_Yes").GetComponent<Button>();
+        var btnYes = GameObject.Find("Canvas/YesNoDialog/Button_Yes").GetComponent<Button>();
         btnYes.onClick.RemoveAllListeners();
-		btnYes.onClick.AddListener(() => {
+        btnYes.onClick.AddListener(() => {
             dialog.SetActive(false);
             yesCallback();
-		});
+        });
 
-		var btnNo = GameObject.Find("Canvas/YesNoDialog/Button_No").GetComponent<Button>();
+        var btnNo = GameObject.Find("Canvas/YesNoDialog/Button_No").GetComponent<Button>();
         btnNo.onClick.RemoveAllListeners();
-		btnNo.onClick.AddListener(() => {
+        btnNo.onClick.AddListener(() => {
             dialog.SetActive(false);
             noCallback();
-		});
-	}
+        });
+    }
 }
