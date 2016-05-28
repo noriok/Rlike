cdc4a39bba0828c23ad0fc988bc44925f654a323
Norio Kobayashi <noriokob@gmail.com>
2016-05-28 13:17:24 +0900

    indent

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -4,33 +4,33 @@ using UnityEngine.UI;
 // using System.Collections;
 
 public class Dialog {
-	public bool IsOpen { get; private set; }
+    public bool IsOpen { get; private set; }
 
-	private GameObject _dialog;
-	private GameObject _dialogText;
+    private GameObject _dialog;
+    private GameObject _dialogText;
 
-	public Dialog() {
-		_dialog = GameObject.Find("Canvas/Dialog");
-		_dialogText = GameObject.Find("Canvas/Dialog/Text");
+    public Dialog() {
+        _dialog = GameObject.Find("Canvas/Dialog");
+        _dialogText = GameObject.Find("Canvas/Dialog/Text");
 
-		var btnOK = GameObject.Find("Canvas/Dialog/Button_OK").GetComponent<Button>();
+        var btnOK = GameObject.Find("Canvas/Dialog/Button_OK").GetComponent<Button>();
 
         btnOK.onClick.RemoveAllListeners();
-		btnOK.onClick.AddListener(() => {
-			Assert.IsTrue(IsOpen);
-			IsOpen = false;
-			_dialog.SetActive(false);
-		});
+        btnOK.onClick.AddListener(() => {
+            Assert.IsTrue(IsOpen);
+            IsOpen = false;
+            _dialog.SetActive(false);
+        });
 
-		_dialog.SetActive(false);
-	}
+        _dialog.SetActive(false);
+    }
 
-	public void Show(string message) {
-		Assert.IsFalse(IsOpen);
+    public void Show(string message) {
+        Assert.IsFalse(IsOpen);
 
-		IsOpen = true;
-		var text = _dialogText.GetComponent<Text>();
-		text.text = message;
-		_dialog.SetActive(true);
-	}
+        IsOpen = true;
+        var text = _dialogText.GetComponent<Text>();
+        text.text = message;
+        _dialog.SetActive(true);
+    }
 }
