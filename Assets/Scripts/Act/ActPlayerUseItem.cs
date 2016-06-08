93f7cab31ae6db9970935689e44a185406648c5a
Norio Kobayashi <noriokob@gmail.com>
2016-06-08 23:27:13 +0900

    indent

--- Assets/Scripts/Act/ActPlayerUseItem.cs
+++ Assets/Scripts/Act/ActPlayerUseItem.cs
@@ -6,17 +6,17 @@ public class ActPlayerUseItem : Act {
     private Player _player;
     private Item _item;
 
-	public ActPlayerUseItem(Player player, Item item) : base(player) {
+    public ActPlayerUseItem(Player player, Item item) : base(player) {
         _player = player;
         _item = item;
-	}
+    }
 
-	protected override IEnumerator RunAnimation(MainSystem sys) {
+    protected override IEnumerator RunAnimation(MainSystem sys) {
         sys.Msg_UseItem(_item);
         yield return _item.Use(Actor, sys);
-	}
+    }
 
-	public override void Apply(MainSystem sys) {
+    public override void Apply(MainSystem sys) {
         _player.RemoveItem(_item);
-	}
+    }
 }
