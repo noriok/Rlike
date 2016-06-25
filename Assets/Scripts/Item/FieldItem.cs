ee49b54842418fce6e3e14e7f5db96ddb9da206b
Norio Kobayashi <noriokob@gmail.com>
2016-06-25 23:59:07 +0900

    indent

--- Assets/Scripts/Item/FieldItem.cs
+++ Assets/Scripts/Item/FieldItem.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 
 public class FieldItem {
-	public Item Item { get; private set; }
-	public Loc Loc { get; private set; }
+    public Item Item { get; private set; }
+    public Loc Loc { get; private set; }
     public Vector3 Position {
         get { return _gobj.transform.position; }
         set { _gobj.transform.position = value; }
     }
 
-	private GameObject _gobj;
+    private GameObject _gobj;
 
-	public FieldItem(Item item, Loc loc, GameObject gobj) {
-		Item = item;
-		Loc = loc;
-		_gobj = gobj;
-		_gobj.transform.position = loc.ToPosition();
-	}
+    public FieldItem(Item item, Loc loc, GameObject gobj) {
+        Item = item;
+        Loc = loc;
+        _gobj = gobj;
+        _gobj.transform.position = loc.ToPosition();
+    }
 
-	public void Destroy() {
-		GameObject.Destroy(_gobj);
-	}
+    public void Destroy() {
+        GameObject.Destroy(_gobj);
+    }
 
     public void UpdateLoc(Loc loc) {
         Loc = loc;
--- Assets/Scripts/UnityExtensions.cs
+++ Assets/Scripts/UnityExtensions.cs
@@ -4,11 +4,11 @@ using System.Collections;
 
 public static class UnityExtensions {
 
-	public static IEnumerator Fade(this MaskableGraphic graphic, float fm, float to, float duration) {
-		var color = graphic.color;
+    public static IEnumerator Fade(this MaskableGraphic graphic, float fm, float to, float duration) {
+        var color = graphic.color;
         yield return CAction.Lerp(duration, fm, to, alpha => {
             color.a = alpha;
             graphic.color = color;
         });
-	}
+    }
 }
