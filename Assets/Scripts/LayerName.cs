7200a326a413871246823ab5127d7985927d764d
Norio Kobayashi <noriokob@gmail.com>
2016-06-25 19:29:04 +0900

    indent

--- Assets/Scripts/LayerName.cs
+++ Assets/Scripts/LayerName.cs
@@ -1,11 +1,11 @@
 ﻿public static class LayerName {
-	public const string FieldObject = "Field Object Layer";
-	public const string Floor = "Floor Layer";
-	public const string Map = "Map Layer";
-	public const string Minimap = "Minimap Layer";
-	public const string Trap = "Trap Layer";
-	public const string Enemy = "Enemy Layer";
-	public const string Item = "Item Layer";
+    public const string FieldObject = "Field Object Layer";
+    public const string Floor = "Floor Layer";
+    public const string Map = "Map Layer";
+    public const string Minimap = "Minimap Layer";
+    public const string Trap = "Trap Layer";
+    public const string Enemy = "Enemy Layer";
+    public const string Item = "Item Layer";
     public const string SpotlightRoom = "Spot Room Layer";
     public const string SpotlightPassage = "Spot Passage Layer";
 
