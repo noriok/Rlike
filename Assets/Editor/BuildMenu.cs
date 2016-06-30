38375a19415f862c0d2a6960318f32ae97f51916
oc_ishikawa <ishikawa@orange-cube.jp>
2016-06-30 19:29:44 +0900

    コマンドラインからビルドできるようにした

---
+++ Assets/Editor/BuildMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+
+public class BuildMenu : EditorWindow
+{
+    [MenuItem("Build/BuildWebPlayer")]
+    static void BuildWebPlayer()
+    {
+        BuildScript.PerformWebBuild();
+    }
+    [MenuItem("Build/iOS")]
+    static void PerformIOSBuild()
+    {
+        BuildScript.PerformIOSBuild();
+    }
+    [MenuItem("Build/android")]
+    static void PerformAndroidBuild()
+    {
+        BuildScript.PerformAndroidBuild();
+    }
+    [MenuItem("Build/windows")]
+    static void PerformWindowsBuild()
+    {
+        BuildScript.PerformWindowsBuild();
+    }
+    [MenuItem("Build/OSX")]
+    static void PerformMacBuild()
+    {
+        BuildScript.PerformMacBuild();
+    }
+}
